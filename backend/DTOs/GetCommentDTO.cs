10580d14e8ddce4fb72207a214f02161b0e6106b
Aleksandr Stepanov <visp.asap@gmail.com>
2026-04-24 15:37:03 +0600

    backend: change uint Reputation to int

--- backend/DTOs/GetCommentDTO.cs
+++ backend/DTOs/GetCommentDTO.cs
@@ -1,11 +1,10 @@
-namespace backend.Models;
+namespace backend.DTOs;
 
-public class Comment
+public class GetCommentDTO
 {
-    public required Guid Guid { get; set; }
     public required Guid PostId { get; set; }
     public required Guid AuthorId { get; set; }
-    public required DateTime PublishDate { get; set; }
+    public required int TimePosted { get; set; }
     public required string CommentText { get; set; } = String.Empty;
     public int Reputation { get; set; }
 }
