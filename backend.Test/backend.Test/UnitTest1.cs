e6461c2b322ab72fd0c7c1a13cbcfdba5a1d0de9
Aleksandr Stepanov <visp.asap@gmail.com>
2026-05-19 12:06:48 +0600

    Add test project to git

---
+++ backend.Test/backend.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using backend;
+
+namespace backend.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
